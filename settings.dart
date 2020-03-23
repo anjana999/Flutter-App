c9d82b4145152625a516a9a348e4f0211b2b94d2
anjana999 <40286602+anjana999@users.noreply.github.com>
2020-03-23 11:03:03 +0530

    Add files via upload

---
+++ settings.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+class settings extends StatelessWidget{
+  @override
+  Widget build(BuildContext){
+    return new Center(child: new Text('settings here'),);
+  }
+}
